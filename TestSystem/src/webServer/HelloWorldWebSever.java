f4df0d0526d22ece04840d2d58ca954a10b73809
intel <intel@m06lab229.legio.local>
2017-01-21 11:12:54 +0900

    вывод Hello worrld

--- TestSystem/src/webServer/HelloWorldWebSever.java
+++ TestSystem/src/webServer/HelloWorldWebSever.java
@@ -1,20 +1,17 @@
 
 package webServer;
-import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
+
 import com.sun.net.httpserver.HttpServer;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.io.PrintWriter;
+
 import java.net.InetSocketAddress;
-import java.net.ServerSocket;
-import java.net.Socket;
+
  
 public class HelloWorldWebSever{
  public static void main(String[] args) throws IOException{
-   HttpServer server=HttpServer.create(new InetSocketAddress(8030),0);
-   server.createContext("/");
-    server.start();
+   HttpServer server=HttpServer.create(new InetSocketAddress(8080),0);
+   server.createContext("/",new HelloController());
+   server.start();
     }
 }
     
